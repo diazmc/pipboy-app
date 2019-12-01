5b7dcc2d3f259f93dcf95346c3d8bdf328891063
diazmc <mc.arthur_d@hotmail.com>
2019-12-01 04:56:57 -0800

    added weapon images

--- js/pipboy.app.js
+++ js/pipboy.app.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
         "range": 119,
         "accuracy": 66,
         "weight": 4.2,
-        "value": 99
+        "value": 99,
+        "img": "../weapons/44_pistol.webp"
         },
         {"name": "10mm_pistol",
         "damage": 18,
@@ -14,7 +15,8 @@ $(document).ready(function() {
         "range": 119,
         "accuracy": 61,
         "weight": 4.2,
-        "value": 53
+        "value": 53,
+        "img": "../weapons/10mm_pistol.webp"
         },
         {"name": "assault_rifle",
         "damage": 30,
@@ -22,7 +24,62 @@ $(document).ready(function() {
         "range": 119,
         "accuracy": 72,
         "weight": 13.1,
-        "value": 144
+        "value": 144,
+        "img": "../weapons/assault_rifle.webp"
+        },
+        {"name": "handmade_rifle",
+        "damage": 32,
+        "fire_rate": 40,
+        "range": 119,
+        "accuracy": 72,
+        "weight": 12.8,
+        "value": 136,
+        "img": "../weapons/handmade_rifle.webp"
+        },
+        {"name": "the_problem_solver",
+        "damage": 57,
+        "fire_rate": 59,
+        "range": 185,
+        "accuracy": 92,
+        "weight": 13.1,
+        "value": 149,
+        "img": "../weapons/the_problem_solver.webp"
+        },
+        {"name": "combat_shotgun",
+        "damage": 50,
+        "fire_rate": 20,
+        "range": 47,
+        "accuracy": 23,
+        "weight": 11.1,
+        "value": 87,
+        "img": "../weapons/combat_shotgun.webp"
+        },
+        {"name": "big_boy",
+        "damage": 486,
+        "fire_rate": 1,
+        "range": 117,
+        "accuracy": 39,
+        "weight": 30.7,
+        "value": 512,
+        "img": "../weapons/big_boy.webp"
+        },
+        {"name": "missile_launcher",
+        "damage": 150,
+        "fire_rate": 2,
+        "range": 203,
+        "accuracy": 65,
+        "weight": 21,
+        "value": 314,
+        "img": "../weapons/missile_launcher.webp"
+        },
+        {"name": "overseers_guardian",
+        "damage": 55,
+        "fire_rate": 90,
+        "range": 109,
+        "accuracy": 76,
+        "weight": 11.1,
+        "value": 117,
+        "img": "../weapons/overseers_guardian.webp"
         }
     ];
 
@@ -47,6 +104,8 @@ $(document).ready(function() {
                 container.find('.accuracy').html(weapons[item].accuracy)
                 container.find('.weight').html(weapons[item].weight)
                 container.find('.value').html(weapons[item].value)
+                var images = $('.weapons')
+                images.find('.weapon-img').attr("src", weapons[item].img)
             }
         };
     });
